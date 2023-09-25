107586bd008a9720fc9705812b99a1db43659170
TomasGnG <tomaskeder2005@gmail.com>
2023-09-25 23:32:26 +0200

    current alertfeature progress

--- src/main/java/de/efi23a/bot/features/AlertFeature.java
+++ src/main/java/de/efi23a/bot/features/AlertFeature.java
@@ -1,19 +1,34 @@
 package de.efi23a.bot.features;
 
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.ListIndexesIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.CreateCollectionOptions;
+import de.efi23a.bot.database.MongoConfig;
 import jakarta.annotation.PostConstruct;
 import java.awt.Color;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import org.bson.Document;
 import org.springframework.stereotype.Component;
+
+import static com.mongodb.client.model.Filters.eq;
 
 /**
  * Alert Feature von Tomas Keder.
@@ -23,15 +38,26 @@ import org.springframework.stereotype.Component;
 public class AlertFeature {
 
   private final JDA jda;
+  private final MongoConfig mongoConfig;
+  private MongoClient client;
+  private MongoDatabase db;
+  private MongoCollection<Document> alerts;
 
   @PostConstruct
   void postConstruct() {
+    client = mongoConfig.mongoClient();
+    db = client.getDatabase("TestCluster");
+    alerts = db.getCollection("alerts");
+
     registerAlertCommand();
+    startAlertCheckerTask();
   }
 
   private void registerAlertCommand() {
     var cmd = Commands.slash("alert", "Verwalte die Erinnerungen.")
         .addSubcommands(
+            new SubcommandData("info", "Zeigt alle Details zu einer Erinnerung.")
+                .addOption(OptionType.STRING, "name", "Name der Erinnerung", true),
             new SubcommandData("list", "Listet alle Erinnerungen auf."),
             new SubcommandData("add", "Erstelle eine neue Erinnerung.")
                 .addOption(OptionType.STRING, "name", "Benenne die Erinnerung.", true)
@@ -44,6 +70,7 @@ public class AlertFeature {
             new SubcommandData("remove", "Entferne eine Erinnerung.")
                 .addOption(OptionType.STRING, "name", "Name der Erinnerung", true),
             new SubcommandData("edit", "Ändere eine Erinnerung")
+                .addOption(OptionType.STRING, "name", "Erinnerung die bearbeitet werden soll.", true)
                 .addOptions(
                     new OptionData(OptionType.STRING, "property", "Eigenschaft die geändert werden soll.")
                         .addChoice("Name", "name")
@@ -61,32 +88,110 @@ public class AlertFeature {
     new Timer().schedule(new TimerTask() {
       @Override
       public void run() {
+        var sdf = new SimpleDateFormat("dd.MM.yyyy");
+        var alerts = getAlerts();
 
+        for(var alert : alerts) {
+          try {
+            var duration = Duration.between(new Date(System.currentTimeMillis()).toInstant(), sdf.parse(alert.getString("date")).toInstant());
+
+            if(!alert.getBoolean("announcement1day") && (duration.get(ChronoUnit.SECONDS)/60/60) < 24) {
+              alert.replace("announcement1day", true);
+              updateAlert(alert);
+              sendAlert(alert);
+            } else if(!alert.getBoolean("announcement3day") && !alert.getBoolean("announcement1day") && (duration.get(ChronoUnit.SECONDS)/60/60) < 72) {
+              alert.replace("announcement3day", true);
+              updateAlert(alert);
+              sendAlert(alert);
+            }
+          } catch (ParseException e) {
+            throw new RuntimeException(e);
+          }
+        }
       }
     }, 1000L, 1000L);
   }
 
   public boolean exists(String name) {
-    return false;
+    return alerts.find(eq("name", name)).first() != null;
   }
 
   public void addAlert(String name, String date, String description, String createdBy) {
-    sendAlert(name, date, description, createdBy);
+    var document = new Document();
+
+    document.put("name", name);
+    document.put("date", date);
+    document.put("description", description);
+    document.put("createdBy", createdBy);
+    document.put("announcement3day", false);
+    document.put("announcement1day", false);
+
+    alerts.insertOne(document);
   }
 
+  private void updateAlert(Document updatedAlert){
+    var filter = eq("_id", updatedAlert.get("_id"));
 
+    if(alerts.find(filter).first() != null) {
+      alerts.replaceOne(filter, updatedAlert);
+    }
+  }
 
-  private void sendAlert(String name, String date, String description, String createdBy) {
+  public void editAlert(String name, String property, String value){
+    var filter = eq("name", name);
+
+    if(exists(name)) {
+      var doc = alerts.find(filter).first();
+      doc.replace(property, value);
+
+      if(property.equalsIgnoreCase("date")) {
+        doc.replace("announcement3day", false);
+        doc.replace("announcement1day", false);
+      }
+
+      alerts.replaceOne(filter, doc);
+    }
+  }
+
+  public void removeAlert(String name) {
+    if(exists(name))
+      alerts.deleteOne(eq("name", name));
+  }
+
+  public FindIterable<Document> getAlerts() {
+    return alerts.find();
+  }
+
+  private Document getAlertByName(String name) {
+    if(exists(name)) {
+      return alerts.find(eq("name", name)).first();
+    }
+    return null;
+  }
+
+  private void sendAlert(Document alert) {
     var embed = new EmbedBuilder()
         .setColor(Color.ORANGE)
         .setTitle("Erinnerung")
-        .addField("Name", name, false)
-        .addField("Datum", date, false)
-        .addField("Beschreibung", description, false)
-        .setFooter("Hinzugefügt von " + createdBy)
+        .addField("Name", alert.getString("name"), false)
+        .addField("Datum", alert.getString("date"), false)
+        .addField("Beschreibung", alert.getString("description"), false)
+        .setFooter("Hinzugefügt von " + alert.getString("createdBy"))
         .build();
 
     jda.getTextChannelById(1155210664084787300L)
         .sendMessage("everyone..").setEmbeds(embed).queue();
+  }
+
+  public MessageEmbed getAlertEmbedMessage(String name) {
+    var alert = getAlertByName(name);
+    return new EmbedBuilder()
+        .setColor(Color.ORANGE)
+        .setTitle("Erinnerung")
+        .addField("Name", alert.getString("name"), false)
+        .addField("Datum", alert.getString("date"), false)
+        .addField("Beschreibung", alert.getString("description"), false)
+        .setFooter("Hinzugefügt von " + alert.getString("createdBy"))
+        .build();
   }
 }
--- src/main/java/de/efi23a/bot/features/AlertFeatureListener.java
+++ src/main/java/de/efi23a/bot/features/AlertFeatureListener.java
@@ -6,10 +6,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.springframework.stereotype.Component;
-import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Duration;
 import java.util.Date;
 
 @Component
@@ -34,6 +31,27 @@ public class AlertFeatureListener extends ListenerAdapter {
     if(subcommand == null)
       return;
 
+    if(subcommand.equalsIgnoreCase("info")) {
+      var name = event.getOption("name").getAsString();
+
+      if(!alertFeature.exists(name)) {
+        event.reply("Eine Erinnerung mit diesem Namen gibt es nicht.").setEphemeral(true).queue();
+        return;
+      }
+
+      event.replyEmbeds(alertFeature.getAlertEmbedMessage(name)).setEphemeral(true).queue();
+    }
+    if(subcommand.equalsIgnoreCase("list")) {
+      var alerts = alertFeature.getAlerts();
+      var builder = new StringBuilder();
+
+      for (var alert : alerts) {
+        builder.append("-> ").append(alert.get("name")).append(System.lineSeparator());
+      }
+
+      event.reply(builder.toString()).setEphemeral(true).queue();
+      return;
+    }
     if(subcommand.equalsIgnoreCase("add")) {
       var name = event.getOption("name").getAsString();
       var date = event.getOption("date").getAsString();
@@ -57,8 +75,47 @@ public class AlertFeatureListener extends ListenerAdapter {
         return;
       }
 
-      alertFeature.addAlert(name, date, description, event.getMember().getEffectiveName());
       event.reply("Du hast eine neue Erinnerung hinzugefügt.").setEphemeral(true).queue();
+      alertFeature.addAlert(name, date, description, event.getMember().getEffectiveName());
+      return;
+    }
+    if(subcommand.equalsIgnoreCase("edit")) {
+      var name = event.getOption("name").getAsString();
+      var property = event.getOption("property").getAsString();
+      var value = event.getOption("value").getAsString();
+
+      if(!alertFeature.exists(name)) {
+        event.reply("Eine Erinnerung mit diesem Namen gibt es nicht.").setEphemeral(true).queue();
+        return;
+      }
+
+      if(property.equalsIgnoreCase("date")) {
+        try {
+          var sdf = new SimpleDateFormat("dd.MM.yyyy");
+
+          var dateInstanz = sdf.parse(value);
+
+          if(sdf.parse(sdf.format(new Date(System.currentTimeMillis()))).after(dateInstanz))
+            throw new Exception();
+        } catch (Exception e) {
+          event.reply("Das eingetragene Datum(``" + value + "``) ist ungültig!").setEphemeral(true).queue();
+          return;
+        }
+      }
+
+      alertFeature.editAlert(name, property, value);
+      event.reply("Die Erinnerung '" + name + "' wurde geändert.").setEphemeral(true).queue();
+    }
+    if(subcommand.equalsIgnoreCase("remove")) {
+      var name = event.getOption("name").getAsString();
+
+      if(!alertFeature.exists(name)) {
+        event.reply("Eine Erinnerung mit diesem Namen gibt es nicht.").setEphemeral(true).queue();
+        return;
+      }
+
+      alertFeature.removeAlert(name);
+      event.reply("Die Erinnerung '" + name + "' wurde gelöscht.").setEphemeral(true).queue();
     }
   }
 }
