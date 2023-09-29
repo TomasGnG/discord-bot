ccae6510c50ffdcb2398a02387ae3b74b01a7056
MCMDEV <mcmdev@mcmdev.de>
2023-09-29 21:44:37 +0200

    refactor: remove javadoc

--- src/main/java/de/efi23a/bot/BotApplication.java
+++ src/main/java/de/efi23a/bot/BotApplication.java
@@ -4,20 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-/**
- * Dient als Einstiegspunkt für die Bot-Anwendung.
- */
-
 @EnableScheduling
 @SpringBootApplication
 public class BotApplication {
 
-  /**
-   * Starten die Bot-Anwendung.
-   * Die Start-Logik wird an das Spring-Framework übergeben.
-   *
-   * @param args Kommandozeilenargumente
-   */
   public static void main(String[] args) {
     SpringApplication.run(BotApplication.class, args);
   }
--- src/main/java/de/efi23a/bot/features/MailForwardingFeature.java
+++ src/main/java/de/efi23a/bot/features/MailForwardingFeature.java
@@ -30,35 +30,18 @@ import org.jetbrains.annotations.Nullable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-/**
- * Die Mailweiterleitungsfunktion kann sich in einen IMAP-Server einloggen und die dort
- * neuen eingegangenen Mails in einen Discord-Textkanal weiterleiten.
- */
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class MailForwardingFeature {
 
-  /**
-   * Der Name des Ordners, in dem die neuen Mails gesucht werden sollen.
-   */
   private static final String INBOX_FOLDER = "INBOX";
-  /**
-   * Der Name der Umgebungsvariable, die die URL des IMAP-Servers enthält.
-   */
+
   private static final String MAIL_URL_VARIABLE = "MAIL_URL";
-  /**
-   * Der Name der Umgebungsvariable, die die Channel-ID des Ziel-Discord-Textkanals enthält.
-   */
+
   private static final String MAIL_CHANNEL_ID = "MAIL_CHANNEL_ID";
   private final JDA jda;
 
-  /**
-   * Validiert regelmäßig die Umgebungsvariablen und leitet neue Mails weiter.
-   *
-   * @throws MessagingException Wird geworfen, wenn eine Mail nicht gelesen werden kann.
-   * @throws IOException        Wird geworfen, wenn der Inhalt einer Mail nicht gelesen werden kann.
-   */
   @Scheduled(fixedDelay = 5, timeUnit = TimeUnit.MINUTES)
   private void runScheduledTask() throws MessagingException, IOException {
     log.info("Running mail task");
@@ -85,15 +68,6 @@ public class MailForwardingFeature {
 
   //<editor-fold desc="Mail Logik">
 
-  /**
-   * Leitet neue Mails aus dem IMAP-Server in den Discord-Textkanal weiter.
-   *
-   * @param session     Die Session, die für die Verbindung zum IMAP-Server verwendet wird.
-   * @param url         Die URL des IMAP-Servers.
-   * @param textChannel Der Textkanal, in den die Mails weitergeleitet werden sollen.
-   * @throws MessagingException Wird geworfen, wenn eine Mail nicht gelesen werden kann.
-   * @throws IOException        Wird geworfen, wenn der Inhalt einer Mail nicht gelesen werden kann.
-   */
   private void handleMailForwarding(@NotNull Session session, @NotNull String url,
                                     @NotNull TextChannel textChannel)
       throws MessagingException, IOException {
@@ -108,24 +82,11 @@ public class MailForwardingFeature {
     }
   }
 
-  /**
-   * Erstellt eine neue Sitzung für die Verbindung zu einem IMAPs-Server.
-   *
-   * @return Die erstellte Sitzung.
-   */
   @NotNull
   public Session createSession() {
     return Session.getInstance(getMailProperties());
   }
 
-  /**
-   * Stellt eine Verbindung zum IMAP-Server her.
-   *
-   * @param session Die Sitzung, die für die Verbindung zum IMAP-Server verwendet wird.
-   * @param url     Die URL des IMAP-Servers.
-   * @return Der geöffnete Store.
-   * @throws MessagingException Wird geworfen, wenn eine Mail nicht gelesen werden kann.
-   */
   @NotNull
   public Store openStore(@NotNull Session session, @NotNull String url) throws MessagingException {
     Store store = session.getStore(new URLName(url));
@@ -133,13 +94,6 @@ public class MailForwardingFeature {
     return store;
   }
 
-  /**
-   * Öffnet den Standard-Posteingang des IMAP-Servers, um neue Mails zu lesen.
-   *
-   * @param store Der Store, der für die Verbindung zum IMAP-Server verwendet wird.
-   * @return Der geöffnete Posteingang.
-   * @throws MessagingException Wird geworfen, wenn eine Mail nicht gelesen werden kann.
-   */
   @NotNull
   public Folder openFolder(@NotNull Store store) throws MessagingException {
     Folder folder = store.getFolder(INBOX_FOLDER);
@@ -147,23 +101,11 @@ public class MailForwardingFeature {
     return folder;
   }
 
-  /**
-   * Sucht nach ungelesenen Mails in einem Ordner.
-   *
-   * @param emailFolder Der Ordner, in dem nach neuen Mails gesucht werden soll.
-   * @return Die Liste der neuen Mails.
-   * @throws MessagingException Wird geworfen, wenn eine Mail nicht gelesen werden kann.
-   */
   @NotNull
   public List<Message> getNewMails(@NotNull Folder emailFolder) throws MessagingException {
     return Arrays.asList(emailFolder.search(new FlagTerm(new Flags(Flags.Flag.SEEN), false)));
   }
 
-  /**
-   * Erstellt die {@link Properties} für die Verbindung zum IMAPs-Server.
-   *
-   * @return Die erstellten Properties.
-   */
   @NotNull
   public Properties getMailProperties() {
     Properties properties = new Properties();
@@ -175,22 +117,12 @@ public class MailForwardingFeature {
 
   //<editor-fold desc="Environment Variables">
 
-  /**
-   * Liest die URL des Ziel-IMAP-Servers aus den Umgebungsvariablen.
-   *
-   * @return Die URL des Ziel-IMAP-Servers.
-   */
   @Nullable
   @Contract(pure = true)
   public String fetchMailUrl() {
     return System.getenv(MAIL_URL_VARIABLE);
   }
 
-  /**
-   * Liest die Channel-ID des Ziel-Discord-Textkanals aus den Umgebungsvariablen.
-   *
-   * @return Die Channel-ID des Ziel-Discord-Textkanals.
-   */
   @Nullable
   @Contract(pure = true)
   public String fetchMailChannelId() {
@@ -200,14 +132,6 @@ public class MailForwardingFeature {
 
   //<editor-fold desc="Embed Building">
 
-  /**
-   * Baut ein {@link MessageEmbed} aus einer {@link Message E-Mail-Nachricht}.
-   *
-   * @param message Die E-Mail-Nachricht, aus der das Embed gebaut werden soll.
-   * @return Das gebaute Embed.
-   * @throws MessagingException Wird geworfen, wenn eine Mail nicht gelesen werden kann.
-   * @throws IOException        Wird geworfen, wenn der Inhalt einer Mail nicht gelesen werden kann.
-   */
   @NotNull
   public MessageEmbed buildEmbed(@NotNull Message message) throws MessagingException, IOException {
     return new EmbedBuilder()
@@ -217,12 +141,6 @@ public class MailForwardingFeature {
         .build();
   }
 
-  /**
-   * Baut einen Adressen-String aus einem Array von Adressen.
-   *
-   * @param addresses Das Array von Adressen, aus dem der String gebaut werden soll.
-   * @return Der gebaute Adressen-String.
-   */
   @NotNull
   private String buildAddressString(@NotNull Address[] addresses) {
     return Arrays.stream(addresses)
@@ -230,14 +148,6 @@ public class MailForwardingFeature {
         .collect(Collectors.joining(", "));
   }
 
-  /**
-   * Baut einen String aus dem Inhalt einer E-Mail-Nachricht.
-   *
-   * @param message Die E-Mail-Nachricht, aus der der String gebaut werden soll.
-   * @return Der gebaute String.
-   * @throws MessagingException Wird geworfen, wenn eine Mail nicht gelesen werden kann.
-   * @throws IOException        Wird geworfen, wenn der Inhalt einer Mail nicht gelesen werden kann.
-   */
   @NotNull
   private String buildBody(@NotNull Message message) throws MessagingException, IOException {
     Object content = message.getContent();
